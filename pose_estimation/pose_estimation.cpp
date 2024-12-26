e3a1a1d6690dc63db7fab596514a7769d02bda20
pkyleEros <kyle.park@ultradent.com>
2024-12-26 16:19:38 -0700

    [adding face pose estimation library]

---
+++ pose_estimation/pose_estimation.cpp
@@ -0,0 +1,9 @@
+//
+// Created by pkyle on 12/26/24.
+//
+
+#include "pose_estimation.h"
+
+namespace re {
+
+} // re
---
+++ pose_estimation/pose_estimation.h
@@ -0,0 +1,16 @@
+//
+// Created by pkyle on 12/26/24.
+//
+
+#ifndef FACE_POSE_POSE_ESTIMATION_POSE_ESTIMATION_H_
+#define FACE_POSE_POSE_ESTIMATION_POSE_ESTIMATION_H_
+
+namespace re {
+
+class PoseEstimation {
+
+};
+
+} // re
+
+#endif //FACE_POSE_POSE_ESTIMATION_POSE_ESTIMATION_H_
